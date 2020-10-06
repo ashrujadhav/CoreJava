e184882b60485773e64a84427d02e5ea3d0b278e
ashrujadhav <65262981+ashrujadhav@users.noreply.github.com>
2020-10-06 21:29:37 +0530

    Update Q31.java

--- Assignment_3/Q31.java
+++ Assignment_3/Q31.java
@@ -1,7 +1,7 @@
 /* 31.	Create a class Student with 2 data members rno and name. Create one method setData() that takes roll number and student name as parameter and stores them in data members rno and name. Create one more method showData() to print the data member values. Create another class ( main class) StudentDemo that creates Student class object and calls
  setData() and showData() methods.import java.util.*;*/
 
-class Student{
+class Q31{
 
 	private int rno;
 	private String name;
